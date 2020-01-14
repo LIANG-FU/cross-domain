11836021716aa2d2ab24eee983abbfcd5e614ece
傅良 <5381362+fu_liang@user.noreply.gitee.com>
2020-01-14 19:27:56 +0800

    first commit

---
+++ JSONP/serverJSONP.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.listen(8001, _ => {
+  console.log("OK");
+});
+app.get("/list", (req, res) => {
+    // Function.prototype是一个匿名的空函数
+  let { callback = Function.prototype } = req.query;
+  let data = {
+      code:0,
+      message:'跨域请求'
+  }
+  res.send(`${callback}(${JSON.stringify(data)})`)
+});
